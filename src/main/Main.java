2412e5569097b227f50f7d10d90ca394707e9235
krzychu1999ryki <krzychu1999ryki@gmail.com>
2020-01-12 19:59:53 +0100

    Revert "Added menu"
    This reverts commit e85a9371dbccdce989aad8ddf3e78994ae08f7b2.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,22 +1,22 @@
 package main;
 
-import view.ViewManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
-
+import view.GameViewManager;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        ViewManager manager = new ViewManager();
-        primaryStage = manager.getMainStage();
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        try{
+            GameViewManager gameManager = new GameViewManager();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
     public static void main(String[] args) {
         launch(args);
     }
-
 }
